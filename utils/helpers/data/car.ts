939eb9207ad3e06770f11e8297feacccf6970d39
Taiwobello <bellotaiwolateef1999@gmail.com>
2022-10-14 10:29:17 +0100

    add car details record

--- utils/helpers/data/car.ts
+++ utils/helpers/data/car.ts
@@ -1,11 +1,11 @@
 import { restAPIInstance } from "../api-config";
-import { Car } from "../types/Car";
+import { Car, CarDetails } from "../types/Car";
 import RequestResponse from "../types/RequestResponse";
 
 export const adaptCarRecord = (record: any, fromBackend?: boolean) => {
   const keyMap = {
     id: "id",
-    image: "imageUrl",
+    imageUrl: "imageUrl",
     year: "year",
     sold: "sold",
     title: "title",
@@ -24,6 +24,64 @@ export const adaptCarRecord = (record: any, fromBackend?: boolean) => {
   }, {});
 
   return adaptedRecord;
+};
+
+const adaptCarDetailsRecord = (record: any, fromBackend?: boolean) => {
+  const keyMap = {
+    id: "id",
+    imageUrl: "imageUrl",
+    year: "year",
+    sold: "sold",
+    title: "title",
+    price: "marketplacePrice",
+    oldPrice: "marketplaceOldPrice",
+    name: "carName",
+    fuelType: "fuelType",
+    model: "model",
+    brand: "brand",
+    brandImageUrl: "brandImageUrl",
+    condition: "sellingCondition",
+    state: "state",
+    city: "city",
+    interiorColor: "interiorColor",
+    exteriorColor: "exteriorColor",
+    country: "country"
+  };
+
+  const processedRecord = {
+    ...record,
+
+    brand: record.model.make.name,
+    brandImageUrl: record.model.make.imageUrl
+  };
+
+  const adaptedRecord = Object.entries(keyMap).reduce((map, arr) => {
+    const value = processedRecord[fromBackend ? arr[1] : arr[0]];
+    return value === undefined || value === ""
+      ? map
+      : {
+          ...map,
+          [fromBackend ? arr[0] : arr[1]]: value
+        };
+  }, {});
+
+  return adaptedRecord;
+};
+
+export const getCar: (id: string) => Promise<RequestResponse<Car>> = async id => {
+  try {
+    const response = await restAPIInstance.get(`/inventory/car/${id}`);
+    return {
+      error: false,
+      data: adaptCarDetailsRecord(response, true) as CarDetails
+    };
+  } catch (err) {
+    return {
+      error: true,
+      message: (err as Error).message,
+      data: null
+    };
+  }
 };
 
 export const getAllCars: () => Promise<RequestResponse<Car[]>> = async () => {
--- utils/helpers/types/Car.ts
+++ utils/helpers/types/Car.ts
@@ -1,9 +1,22 @@
 export interface Car {
   id: string;
-  image: string;
+  imageUrl: string;
   year: number;
   sold: boolean;
   title: string;
   price: number;
   oldPrice: number;
+}
+
+export interface CarDetails extends Car {
+  brand: string;
+  brandImageUrl: string;
+  fuelType: string;
+  name: string;
+  condition: string;
+  state: string;
+  city: string;
+  interiorColor: string;
+  exteriorColor: string;
+  country: string;
 }
